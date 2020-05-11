27c5dcee028e2ad339bdab5dc969938c0060c601
Jake Kwak <jakejaekwak@gmail.com>
2020-05-11 22:11:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,3 +9,11 @@ def loop_message_five_times(string)
     i += 1
   end
 end
+
+def loop_message_n_times(string,integer)
+  count = 0
+  while count < integer do
+    puts string
+    count += 1
+  end
+end
