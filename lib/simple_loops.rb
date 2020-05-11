39a7cc8d7e45a451e83d9816489a4f89afd77f17
Jake Kwak <jakejaekwak@gmail.com>
2020-05-11 22:15:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,3 +17,19 @@ def loop_message_n_times(string,integer)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count].to_s
+    count +=1
+  end
+end
